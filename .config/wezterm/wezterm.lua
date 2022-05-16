5603d7a0801f631dc0e9c2755202bb79615a8c29
rudeigerc <rudeigerc@gmail.com>
2022-05-17 01:18:09 +0800

    wezterm: update key mappings and hyperlinks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,5 +9,32 @@ return {
   keys = {
     {key="LeftArrow", mods="SUPER", action=wezterm.action{ActivateTabRelative=-1}},
     {key="RightArrow", mods="SUPER", action=wezterm.action{ActivateTabRelative=1}},
+    {key="d", mods="SUPER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    {key="d", mods="SUPER|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+    {key="c", mods="SUPER|SHIFT", action="ActivateCopyMode"},
+    {key="LeftArrow", mods="SUPER|ALT", action=wezterm.action({ActivatePaneDirection="Left"})},
+    {key="RightArrow", mods="SUPER|ALT", action=wezterm.action({ActivatePaneDirection="Right"})},
+    {key="UpArrow", mods="SUPER|ALT", action=wezterm.action({ActivatePaneDirection="Up"})},
+    {key="DownArrow", mods="SUPER|ALT", action=wezterm.action({ActivatePaneDirection="Down"})},
+  },
+  hyperlink_rules = {
+    -- Linkify things that look like URLs
+    -- This is actually the default if you don't specify any hyperlink_rules
+    {
+      regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
+      format = "$0",
+    },
+
+    -- linkify email addresses
+    {
+      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+      format = "mailto:$0",
+    },
+
+    -- file:// URI
+    {
+      regex = "\\bfile://\\S*\\b",
+      format = "$0",
+    },
   },
 }
